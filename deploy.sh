788d13ae1c40ac17c21aad2f0372975e5112a1c7
amanharitsh123 <amanharitsh123@gmail.com>
2018-07-11 15:22:05 +0530

    Sudo added

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-apt-get -y update
-apt-get -y upgrade
-
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install git
 #Cloning Sugarizer
 git clone https://github.com/llaske/sugarizer
 git clone https://github.com/llaske/sugarizer-server
 
 #Docker Install
-curl -fsSL https://get.docker.com/ | sh
-curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -fsSL https://get.docker.com/ | sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 #Starting Server up
 cd sugarizer-server
